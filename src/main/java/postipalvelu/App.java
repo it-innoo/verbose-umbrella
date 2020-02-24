f3133fdae0726c3c4274554a7864e02bc3915c78
jahlgren <jukka.ahlgren@gmail.com>
2020-02-24 10:54:26 +0200

    Add Code Climate Checkstyle

--- src/main/java/postipalvelu/App.java
+++ src/main/java/postipalvelu/App.java
@@ -3,19 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Postipalvelu;
+package postipalvelu;
 
 /**
  *
  * @author jukka
  */
-public class App {
+public final class App {
+
+    private App() {
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // TODO code application logic here
+        System.out.println("Hello gradle!");
     }
     
 }
